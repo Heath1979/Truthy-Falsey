f63feb40953582294e52912950a6fab895694e1a
Sarah Moorby <lhijd8vq@students.codeinstitute.net>
2024-10-23 09:06:21 +0000

    shuffle the array in the js

--- assets/js/script.js
+++ assets/js/script.js
@@ -69,3 +69,9 @@ document.getElementById('next').onclick = () => {
     currentQuestionIndex++;
     displayQuestion();
 };
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
