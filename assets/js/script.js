2b2beca85f7db2921aaae4d6eea2291fd8da954e
Sarah Moorby <lhijd8vq@students.codeinstitute.net>
2024-10-23 08:31:41 +0000

    add js to get api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
-https://opentdb.com/api.php?amount=10&category=27&difficulty=easy&type=boolean
-
-
+const apiUrl = 'https://opentdb.com/api.php?amount=10&category=27&difficulty=easy&type=boolean';
+let currentQuestionIndex = 0;
+let score = 0;
+let questions = [];
 
 async function fetchQuestions() {
     const response = await fetch(apiUrl);
